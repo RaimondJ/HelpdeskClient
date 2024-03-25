bf2d1b49cb4c8406ead505076aa080c898da390d
timecold <timecold2@enigmaps.eu>
2024-03-26 00:01:50 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './scss/style.scss';
+import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import Header from './Header.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <Header />
+    </>
+  )
+}
+
+export default App
